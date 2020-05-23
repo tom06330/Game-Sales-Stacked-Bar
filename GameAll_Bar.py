52d6b02948e4ae48b4b225bf749931fd91cc591c
tom06330 <34601438+tom06330@users.noreply.github.com>
2020-05-23 19:02:33 +0800

    Update GameAll_Bar.py

--- GameAll_Bar.py
+++ GameAll_Bar.py
@@ -5,12 +5,14 @@ import pandas as pd
 from plotly import graph_objects as go
 import os
 
+#讀取個主機平台遊戲銷售csv檔
 df1 = pd.read_csv("GamePS4.csv")
 df2 = pd.read_csv("GameXOne.csv")
 df3 = pd.read_csv("GameNS.csv")
 
 GameAll_Bar = dash.Dash()
 
+#建立各主機遊戲銷售長條圖
 PS4 = go.Bar(
     x = df1.game,
     y = df1.TotalSales,
@@ -29,14 +31,15 @@ NS = go.Bar(
     name='NS'
 )
 
-server = GameAll_Bar.server
+server = GameAll_Bar.server  #For Heroku Deploy
 
 GameAll_Bar.layout = html.Div([
     dcc.Graph(id='Game sales Chart',
               figure=go.Figure(data=[PS4, XOne, NS],
-                               layout=go.Layout(title = 'Game Sales Chart', barmode='stack'))
+                               layout=go.Layout(title = 'Game Sales Chart', barmode='stack'))  #建立圖表標題及設定推疊長條圖
               )
     ])
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     GameAll_Bar.run_server(host='0.0.0.0',debug=True,port=port)
